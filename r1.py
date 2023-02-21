26f1f58d29b1ea6d5cc7b20c78e952b90a5e2343
Sam <nqbviwdpm@gmail.com>
2023-02-21 09:53:12 +0800

    done with r1.py

---
+++ r1.py
@@ -0,0 +1,35 @@
+# 讀取檔案
+def read_file(filename):
+	lines = []
+	with open(filename, 'r', encoding='utf-8-sig') as f:
+		for line in f:
+			lines.append(line.strip())
+	return lines
+
+# 格式改寫
+def convert(lines):
+	new = []
+	person = None
+	for line in lines:
+		if line == 'Allen':
+			person = 'Allen'
+			continue
+		elif line == 'Tom':
+			person = 'Tom'
+			continue
+		if person:
+			new.append(person + '： ' + line)
+	return new
+
+# 寫入檔案
+def write_file(filename, lines):
+	with open(filename, 'w', encoding='utf-8-sig') as f:
+		for line in lines:
+			f.write(line + '\n')
+
+def main():
+	lines = read_file('input.txt')
+	lines = convert(lines)
+	write_file('output.txt', lines)
+main()
+
